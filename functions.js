5c5279925fd94fbd7132fe58727cfa8800de97ab
Luis Gomez <49451508+LuiDEVBubble@users.noreply.github.com>
2020-11-21 01:06:29 -0500

    When there is no value inputed by user, the display will not retun 'undefined.' It now breaks out of the function 'solve()'

--- functions.js
+++ functions.js
@@ -3,20 +3,29 @@ function display(val)
 {
     document.getElementById("displayResult").value +=val;
  }
+ 
 //function for evaluation
 function solve()
 {
 
+    //Grabs the value from the element
     let x = document.getElementById("displayResult").value;
-    document.getElementById("previousResult").value = x;
-    // if (x == null)
-    // {
-    //     return;
-    // }
-    let y = eval(x);
-    
-    document.getElementById("displayResult").value = y;
+
+    //Checks to see if you've already put a calculation inside.
+    if ( x == "")
+    {
+        // alert("Please insert a number first");
+        return;
+    }
+    else{
+        document.getElementById("previousResult").value = x;
+        
+        let y = eval(x);
+        
+        document.getElementById("displayResult").value = y;
+    }
 }
+
 //function for clearing the display
 function clr()
 {
